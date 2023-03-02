a8b5525fb461b5bc478ff1d2c5d01b832651df0f
Mmomenzadeh <m.momenzadeh1999@gmail.com>
2023-03-02 18:12:02 +0330

    fix some errors

--- src/Components/Nav/nav.components.jsx
+++ src/Components/Nav/nav.components.jsx
@@ -31,11 +31,13 @@ export const Nav = () => {
         <MdOutlinePhoneInTalk className="nav__link__icons" />
         <span className="nav__link__item">تماس با ما</span>
       </div>
+
+
+
       <div className="nav__contact-us">
         <div className="nav__contact-us__QA">سوالی دارید ؟ تماس بگرید</div>
         <p style={{fontSize:"1.3rem"}}><span style={{color:"#ef3a4f"}}>071</span>-36322050</p>
         <div className="nav__contact-us__icon">
-
         <MdOutlinePhoneInTalk className="nav__link__icons" />
         </div>
 
