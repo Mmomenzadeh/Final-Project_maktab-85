60c696367f2bb23950b0d00e58071f516c9c6d84
Mmomenzadeh <m.momenzadeh1999@gmail.com>
2023-03-03 18:47:21 +0330

    add  dynamic routes

---
+++ src/Config/RoutesConstant/RouteConstant.config.jsx
@@ -0,0 +1,14 @@
+export const HOME ="/";
+export const BLOG ="blog";
+export const ALLPRODUCTS ="allProducts";
+export const LOGIN ="login";
+export const SINGLEPRODUCT ="singleProduct";
+export const BASKETSHOPPING ="basketShopping";
+export const PAYMENT="pyment";
+export const PANLEMANAGEMENT ="panleManagement";
+export const ORDERS ="orders";
+export const NOTFOUND = "notFound";
+export const CONTACTUS ="contactUs";
+export const CHECKOUT ="checkOut";
+export const ABOUT ="about";
+
