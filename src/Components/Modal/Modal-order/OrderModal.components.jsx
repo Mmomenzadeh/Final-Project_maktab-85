085ff7954b875afbb4c1f3f5831275e0ff4986c5
Mmomenzadeh <m.momenzadeh1999@gmail.com>
2023-03-14 10:12:41 +0330

    create modal

---
+++ src/Components/Modal/Modal-order/OrderModal.components.jsx
@@ -0,0 +1,11 @@
+import "../../../Assets/Styles/Components/Modal/index.scss"
+export const OrderModal = () => {
+  return (
+    <div id="modal">
+    <div className="background"></div>
+    <div className="modal-ui">
+      <div className="btn-container"></div>
+    </div>
+  </div>
+  )
+}
