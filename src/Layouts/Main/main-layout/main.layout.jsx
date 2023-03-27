d2d83e61a693356ba2b3dd8b7c3cb33a3c7e6a9a
Mmomenzadeh <m.momenzadeh1999@gmail.com>
2023-03-27 19:04:22 +0430

    add bbaner to home page

--- src/Layouts/Main/main-layout/main.layout.jsx
+++ src/Layouts/Main/main-layout/main.layout.jsx
@@ -1,4 +1,4 @@
-import { Banner, HeadingTitle, ProductsList } from "Components";
+import { Banner, BannerHorizontal, HeadingTitle, ProductsList } from "Components";
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { fetchProducts } from "Redux/Slices/ProductSlice";
@@ -17,11 +17,15 @@ export const MainLayout = () => {
   return (
     <main className="main ">
       <Banner />
+    <BannerHorizontal img="https://www.uplooder.net/img/image/24/318fe923043061fb62ada4804f71c8ac/822172e5b87c21b201cb03198ca83d051409996d-1677824539.jpg" size ="full" className="space" />
       <HeadingTitle categoeyId="1" size="115rem" titleEn="Mobiles" titleFa="گوشی موبایل " />
       <ProductsList productData={mobiles} />
+
+      <BannerHorizontal img="https://www.uplooder.net/img/image/50/935a3aebaeeb5d8f025da5550083c256/laptop-asus.gif" size ="full"  className="space"/>
       <HeadingTitle categoeyId="2" size="120rem" titleEn="Laptops" titleFa=" لپ تاپ " />
       <ProductsList productData={laptops} />
-
+     
+      <BannerHorizontal img="https://www.uplooder.net/img/image/17/52ecc48c86d418c82b8754db5f23c6ee/6a45272b7e841c93f80c3c075ed210db4288180f-1677415939.jpg" size ="full"  className="space"/>
       <HeadingTitle
         categoeyId="3"
         size="100rem"
@@ -29,6 +33,9 @@ export const MainLayout = () => {
         titleFa=" هدفون و هندزفری "
       />
       <ProductsList productData={headPhones} />
+
+      <BannerHorizontal img="https://dkstatics-public.digikala.com/digikala-adservice-banners/5668dea95a68668f943d325aff14e34f7bf9c9e9_1673600968.jpg?x-oss-process=image/quality,q_95/format,webp" size ="full" className="space" />
+
 
       <HeadingTitle
         categoeyId="4"
