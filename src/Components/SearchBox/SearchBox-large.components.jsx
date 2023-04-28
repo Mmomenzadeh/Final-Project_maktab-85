07a4d0037a6b02cbdffcf00a4099caae84449929
Mmomenzadeh <m.momenzadeh1999@gmail.com>
2023-04-28 18:43:45 +0430

    perf: delete select category on search box

--- src/Components/SearchBox/SearchBox-large.components.jsx
+++ src/Components/SearchBox/SearchBox-large.components.jsx
@@ -8,14 +8,7 @@ export const SearchBoxLarge = ({showSearchBox , setShowSearchBox}) => {
   return (
     <div className="searchBoxLarge">
       {showSearchBox ? <Search  setShowSearchBox={setShowSearchBox} showSearchBox={showSearchBox}/> : null}
-      <select className="searchBoxLarge__select">
-        <option value="" className="searchBoxLarge__select__opt">انتخاب دسته بندی </option>
-        <option value="1" className="searchBoxLarge__select__opt">گوشی موبایل</option>
-        <option value="2" className="searchBoxLarge__select__opt">لپ تاپ</option>
-        <option value="3" className="searchBoxLarge__select__opt">هدفون و هنذفری</option>
-        <option value="4" className="searchBoxLarge__select__opt">کنسول بازی</option>
-
-      </select>
+     
       <Input
         type="search "
         holder="جستجوی محصولات"
